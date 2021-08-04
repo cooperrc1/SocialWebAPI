b3119e28ccab74fafa2e09cb2d18195914cca879
cooperrc <coopercutbirth@yahoo.com>
2021-08-03 23:30:50 -0500

    first commit set up routes etc

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const { Schema, model } = require('mongoose');
+let userSchema = new Schema (
+    {
+       
+    }
+);
+userSchema.virtual('friendCount').get(function(){
+    return this.friends.length;
+});
+
+let User = model('User', userSchema);
+module.exports = User;
